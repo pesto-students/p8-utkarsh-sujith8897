159757de8e8f7454f70bd1ea86bd10f3277ab7a6
sujith8897 <sujithkumar8897@gmail.com>
2023-02-04 22:02:19 +0530

    Completed exercise 7.4

---
+++ Week-7/4. Parenthesis Checker.js
@@ -0,0 +1,22 @@
+// Time: O(n), Space: O(1)
+function ParenthesisChecker(s) {
+  let arr = [];
+  let match = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      arr.push(s[i]);
+    } else {
+      if (arr.length && arr[arr.length - 1] === match[s[i]]) {
+        arr.pop();
+      } else return false;
+    }
+  }
+  return arr.length === 0;
+}
+
+// leetcode: https://leetcode.com/problems/valid-parentheses/
