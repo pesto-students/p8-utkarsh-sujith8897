6b2d81183784c402d4f47322ae88a1d26117fdb9
sujith8897 <sujithkumar8897@gmail.com>
2023-02-04 15:09:20 +0530

    completed exercise 6.5

---
+++ Week-6/5. Pair With Given Difference.js
@@ -0,0 +1,17 @@
+function PairDiff(arr, k) {
+  const m = {};
+  for (let i = 0; i < arr.length; i++) {
+    m[arr[i]] = 1;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (m[k + arr[i]]) return 1;
+  }
+  return 0;
+}
+
+const arr1 = [5, 10, 3, 2, 50, 80];
+const k1 = 78;
+console.log(PairDiff(arr1, k1));
+const arr2 = [-10, 20];
+const k2 = 30;
+console.log(PairDiff(arr2, k2));
